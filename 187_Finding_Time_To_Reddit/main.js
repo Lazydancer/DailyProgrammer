cd95478240bc819daea3a6fedd5e29f9313615a0
Lazydancer <jrpucula@gmail.com>
2014-11-06 17:12:39 -0500

    Formatting

--- 187_Finding_Time_To_Reddit/main.js
+++ 187_Finding_Time_To_Reddit/main.js
@@ -1,8 +1,6 @@
-    //Import
-    var list = require("fs").readFileSync("times.txt", "utf8").split("\n");
+    var list = require('fs').readFileSync('times.txt', 'utf8').split("\n");
     list.pop()
 
-    //Convert
     function Entry(date, start, end, name) {
       this.date = date;
       this.start = start;
@@ -10,28 +8,23 @@
       this.name = name;
     }
 
-    list = list.map(function createEntry(elem){//createEntry
-    //String -> Object
-      var date;
-
-      if (elem[4] !== "-"){
-        date = elem[3] + elem[4];
-        //Removes one elem to line up for times and name 
+    list = list.map(function(elem){//createEntry
+  
+      if (elem[4] === '-'){
+        var date = parseInt(elem[3], 10); 
+      }else{
+        var date = parseInt(elem[3] + elem[4], 10);
         elem = elem.slice(0,3) + elem.slice(4, elem.length)
-      }else 
-        date = elem[3]; 
+      }
 
-      date = parseInt(date);
-      
-      var start = elem.slice(11, 13) + elem.slice(14, 16);
-      start = parseInt(start);
-      if (elem[17] === "P" && start<1200) start += 1200;
-     
-      var end = elem.slice(23, 25) + elem.slice(26,28);
-      end = parseInt(end);
-      if (elem[29] === "P" && end<1200) end += 1200;
+      var start = parseInt(elem.slice(11, 13) + elem.slice(14, 16), 10);
+
+      var end = parseInt(elem.slice(23, 25) + elem.slice(26,28), 10);
 
       var name = elem.slice(35, elem.length);
+
+      if (elem[17] === 'P' && start<1200) start += 1200;
+      if (elem[29] === 'P' && end<1200) end += 1200;
       
       return new Entry(date, start, end, name);
     });
@@ -40,13 +33,13 @@
       return (a.date * 1e5 + a.start) - (b.date * 1e5 + b.start);
     });
 
-    list = function(list){//breakIntoDays
-    //[Object] -> [[Object]]
-      var newList = [];
-      var day = [];
-      var temp = list[0].date;
-      for(var i = 0; i<list.length; i += 1){
-        if(temp != list[i].date || i == list.length-1){
+    list = function(list) {//breakIntoDays
+      var newList = [],
+          day = [],
+          temp = list[0].date;
+
+      for(var i = 0; i<list.length; i += 1) {
+        if(temp != list[i].date || i == list.length-1) {
           newList.push(day);
           day = [];
           temp = list[i].date;
@@ -57,66 +50,63 @@
     }(list);
 
     list = list.map(function(day){//addReddit
-    //[Object] -> [Object] 
-      var reddit = { date: day[0].date,
-                    start: 0, end: 0,
-                    name: "reddit" };
-      var diff,large = 0,largeI; 
+      var reddit = { date: day[0].date, name: 'reddit' },
+          large = 0,
+          largeIndex,
+          diff; 
 
-      for(var i = 1; i < day.length; i++){
+      for(var i = 1; i < day.length; i += 1){
         diff = day[i].start - day[i-1].end;
-        if( diff > large ){
+        if(diff > large) {
           large = diff;
-          largeI = i;
+          largeIndex = i;
         }
       }
      
-      reddit.start = day[largeI -1].end;
-      reddit.end = day[largeI].start;
+      reddit.start = day[largeIndex -1].end;
+      reddit.end = day[largeIndex].start;
 
-      day.splice(largeI, 0, reddit);
+      day.splice(largeIndex, 0, reddit);
       return day;
     });
 
 
     list.forEach(function(elem) { //printSchedule
-      console.log("\nOctober " + elem[0].date + " 2014");
-      console.log("----------------------\n");
+      console.log('\nOctober ' + elem[0].date + ' 2014\n----------------------\n');
       
       elem.forEach(function(el) {
-        console.log( el.start + " - " + el.end + ": " + el.name ); 
+        console.log( el.start + ' - ' + el.end + ': ' + el.name ); 
+      });
+    });
+
+
+    function activities(list){
+      var activites = [] 
+      var total = 0;
+
+      list = [].concat.apply([],list); //flatten
+
+      list.forEach(function(elem) { //fillActivites
+        var loc = arrayObjectIndexOf(activites, elem.name, 'name')
+
+        if(loc !== -1) activites[loc].time += (elem.end - elem.start)
+        else activites.push({name: elem.name, time: elem.end - elem.start});
       });
 
-    });
-
-
-    list = [].concat.apply([],list); //flatten
-
-    var activites = [] 
-
-    list.forEach(function(elem) { //fillActivites
-      var loc = arrayObjectIndexOf(activites, elem.name, "name")
-
-      if(loc !== -1)
-        activites[loc].time += (elem.end - elem.start)
-      else
-        activites.push({name: elem.name, time: elem.end - elem.start});
-    });
-
-    function arrayObjectIndexOf(array, searchTerm, property){
-      for(var i = 0; i < array.length; i += 1){
-        if (array[i][property] === searchTerm) return i;
+      function arrayObjectIndexOf(array, searchTerm, property){
+        for(var i = 0; i < array.length; i += 1){
+          if (array[i][property] === searchTerm) return i;
+        }
+        return -1;
       }
-      return -1;
-    }
 
-    var total = 0;
-    activites.forEach(function(elem){ total += elem.time; });
+      activites.forEach(function(elem){ total += elem.time; });
 
-    activites.sort(function(a,b){return b.time - a.time;});
+      activites.sort(function(a,b){return b.time - a.time;});
 
-    console.log("\nTime Spent\n-------------------------\n");
-    activites.forEach(function(elem){
-      var percent = Math.floor(100*(elem.time/total));
-      console.log(elem.name + ": " + elem.time + " min (" + percent + "%)");
-    });
+      console.log('\nTime Spent\n-------------------------\n');
+      activites.forEach(function(elem){
+        var percent = Math.floor(100*(elem.time/total));
+        console.log(elem.name + ': ' + elem.time + ' min (' + percent + '%)');
+      });
+    }(list);
